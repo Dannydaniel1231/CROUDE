91bcca054439bb534e3734b68866ea74f095364b
Dannydaniel1231 <ndaniel4040@gmail.com>
2022-09-07 13:19:15 +0200

    third commit on index.js push to danny

--- index.js
+++ index.js
@@ -9,8 +9,9 @@ if(selectdRow === null){
     insertNewRecord(formData);
 }
 else{
-     
+     updateRecord(formData);
 }
+resetForm();
 }
 function readFormData(){
     var formData = {};
@@ -36,5 +37,38 @@ function readFormData(){
      var cell4 = newRow.insertCell(3);
      cell4.innerHTML = data.perPrice;
      var cell5 = newRow.insertCell(4);
-     cell5.innerHTML =`<button>edit</button> <button>delete</button>`
+     cell5.innerHTML =`<button onclick="onEdit(this)">edit</button> <button onClick= "onDelete(this)">delete</button>`
  } 
+
+
+ function onEdit(td){
+    selectedRow = td.parentElement.parentElement;
+    document.getElementById("productCode").value = selectdRow.cells[0].innerHTML;
+    document.getElementById("product").value = selectdRow.cells[1].innerHTML;
+    document.getElementById("qty").value = selectdRow.cells[2].innerHTML;
+    document.getElementById("perPrice").value = selectdRow.cells[3].innerHTML;
+ }
+
+ function updateRecord(formData){
+    selectedRow = td.parentElement.parentElement;
+     selectdRow.cells[0].innerHTML.formData.productCode;
+     selectdRow.cells[1].innerHTML.formData.product;
+    selectdRow.cells[2].innerHTML = formData.qty;
+    selectdRow.cells[3].innerHTML = formData.perPrice;
+ }
+
+ function onDelete(td){
+    if(confirm("Do u want to delete this record?")){
+        row = td.parentElement.parentElement;
+        document.getElementById("storeList").deleteRow(row.rowIndex);
+    }
+    resetForm();
+ }
+ function resetForm(){
+    selectedRow = td.parentElement.parentElement;
+    document.getElementById("productCode").value = "";
+    document.getElementById("product").value = "";
+    document.getElementById("qty").value = "";
+    document.getElementById("perPrice").value = "";
+ }
+
